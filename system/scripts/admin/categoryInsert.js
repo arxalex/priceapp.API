7b9385f74ad8bcb92290f0c0237362417cc5006d
Oleksandr Yushchenko <arxalex@arxalex.com>
2022-02-22 20:31:25 +0200

    fix style

--- system/scripts/admin/categoryInsert.js
+++ system/scripts/admin/categoryInsert.js
@@ -1,67 +1,69 @@
 Vue.component('categoryInsert', {
     template: `
-        <div class="bg-white position-fixed p-4 window-insert shadow-lg rounded">
-            <div class="d-flex mb-3"><h5 class="ms-1 fw-bold flex-fill">Category</h5>
-                <button class="btn mt-0 pt-0 px-0" @click="close">
-                    <i class="bi bi-x text-danger"></i>
-                </button>
-            </div>
-            <div class="d-flex mb-3">
-                <div class="position-relative flex-fill me-2">
-                    <label class="ms-2 px-1 fw-light bg-white position-absolute input-label">Id</label>
-                    <input class="form-control" v-model="category.id" placeholder="Id">
+        <div class="position-fixed window-insert">
+            <div class="bg-white p-4 shadow-lg rounded">
+                <div class="d-flex mb-3"><h5 class="ms-1 fw-bold flex-fill">Category</h5>
+                    <button class="btn mt-0 pt-0 px-0" @click="close">
+                        <i class="bi bi-x text-danger"></i>
+                    </button>
                 </div>
-                <div class="position-relative flex-fill me-2">
-                    <label class="ms-2 px-1 fw-light bg-white position-absolute input-label">Label</label>
-                    <input class="form-control" v-model="category.label" placeholder="Label">
+                <div class="d-flex mb-3">
+                    <div class="position-relative flex-fill me-2">
+                        <label class="ms-2 px-1 fw-light bg-white position-absolute input-label">Id</label>
+                        <input class="form-control" v-model="category.id" placeholder="Id">
+                    </div>
+                    <div class="position-relative flex-fill me-2">
+                        <label class="ms-2 px-1 fw-light bg-white position-absolute input-label">Label</label>
+                        <input class="form-control" v-model="category.label" placeholder="Label">
+                    </div>
+                    <div class="position-relative flex-fill me-2">
+                        <label class="ms-2 px-1 fw-light bg-white position-absolute input-label">Parent</label>
+                        <select class="form-select" v-model="category.parent">
+                            <option disabled value="">Chose parent category</option>
+                            <option v-for="categoryL in categories" :value="categoryL.id">{{ categoryL.label }}</option>
+                        </select>
+                    </div>
+                    <div class="position-relative mx-2 mt-2">
+                        <label class="fw-light fs-min bg-white position-absolute input-label">isFilter</label>
+                        <input class="form-check-input mx-2" type="checkbox" v-model="category.isFilter">
+                    </div>
                 </div>
-                <div class="position-relative flex-fill me-2">
-                    <label class="ms-2 px-1 fw-light bg-white position-absolute input-label">Parent</label>
-                    <select class="form-select" v-model="category.parent">
-                        <option disabled value="">Chose parent category</option>
-                        <option v-for="categoryL in categories" :value="categoryL.id">{{ categoryL.label }}</option>
-                    </select>
+                <h5 class="ms-1 fw-bold mb-3">Category link</h5>
+                <div class="d-flex mb-3">
+                    <div class="position-relative flex-fill me-2">
+                        <label class="ms-2 px-1 fw-light bg-white position-absolute input-label">Id</label>
+                        <input class="form-control" v-model="categoryLink.id" placeholder="Id">
+                    </div>
+                    <div class="position-relative flex-fill me-2">
+                        <label class="ms-2 px-1 fw-light bg-white position-absolute input-label">Category</label>
+                        <select class="form-select" v-model="categoryLink.categoryid">
+                            <option disabled>Chose category</option>
+                            <option :value="null">Null</option>
+                            <option v-for="categoryL in categories" :value="categoryL.id">{{ categoryL.label }}</option>
+                        </select>
+                    </div>
+                    <div class="position-relative flex-fill me-2">
+                        <label class="ms-2 px-1 fw-light bg-white position-absolute input-label">Shop</label>
+                        <select class="form-select" v-model="categoryLink.shopid">
+                            <option disabled>Chose shop</option>
+                            <option :value="null">Null</option>
+                            <option v-for="shop in shops" :value="shop.id">{{ shop.label }}</option>
+                        </select>
+                    </div>
+                    <div class="position-relative flex-fill me-2">
+                        <label class="ms-2 px-1 fw-light bg-white position-absolute input-label">Category shop id</label>
+                        <input class="form-control" v-model="categoryLink.categoryshopid">
+                    </div>
+                    <div class="position-relative flex-fill">
+                        <label class="ms-2 px-1 fw-light bg-white position-absolute input-label">Shop category label</label>
+                        <input class="form-control" v-model="categoryLink.shopcategorylabel">
+                    </div>
                 </div>
-                <div class="position-relative mx-2 mt-2">
-                    <label class="fw-light fs-min bg-white position-absolute input-label">isFilter</label>
-                    <input class="form-check-input mx-2" type="checkbox" v-model="category.isFilter">
+                <div class="input-group">
+                    <button class="btn btn-secondary form-control" v-on:click='insertAndUpdate(1)'>Insert category</button>
+                    <button class="btn btn-secondary form-control" v-on:click='insertAndUpdate(2)'>Update or insert link</button>
+                    <button class="btn btn-primary form-control" v-on:click='insertAndUpdate(3)'>Insert and Update</button>
                 </div>
-            </div>
-            <h5 class="ms-1 fw-bold mb-3">Category link</h5>
-            <div class="d-flex mb-3">
-                <div class="position-relative flex-fill me-2">
-                    <label class="ms-2 px-1 fw-light bg-white position-absolute input-label">Id</label>
-                    <input class="form-control" v-model="categoryLink.id" placeholder="Id">
-                </div>
-                <div class="position-relative flex-fill me-2">
-                    <label class="ms-2 px-1 fw-light bg-white position-absolute input-label">Category</label>
-                    <select class="form-select" v-model="categoryLink.categoryid">
-                        <option disabled>Chose category</option>
-                        <option :value="null">Null</option>
-                        <option v-for="categoryL in categories" :value="categoryL.id">{{ categoryL.label }}</option>
-                    </select>
-                </div>
-                <div class="position-relative flex-fill me-2">
-                    <label class="ms-2 px-1 fw-light bg-white position-absolute input-label">Shop</label>
-                    <select class="form-select" v-model="categoryLink.shopid">
-                        <option disabled>Chose shop</option>
-                        <option :value="null">Null</option>
-                        <option v-for="shop in shops" :value="shop.id">{{ shop.label }}</option>
-                    </select>
-                </div>
-                <div class="position-relative flex-fill me-2">
-                    <label class="ms-2 px-1 fw-light bg-white position-absolute input-label">Category shop id</label>
-                    <input class="form-control" v-model="categoryLink.categoryshopid">
-                </div>
-                <div class="position-relative flex-fill">
-                    <label class="ms-2 px-1 fw-light bg-white position-absolute input-label">Shop category label</label>
-                    <input class="form-control" v-model="categoryLink.shopcategorylabel">
-                </div>
-            </div>
-            <div class="input-group">
-                <button class="btn btn-secondary form-control" v-on:click='insertAndUpdate(1)'>Insert category</button>
-                <button class="btn btn-secondary form-control" v-on:click='insertAndUpdate(2)'>Update or insert link</button>
-                <button class="btn btn-primary form-control" v-on:click='insertAndUpdate(3)'>Insert and Update</button>
             </div>
         </div>
     `,
--- system/scripts/admin/packageInsert.js
+++ system/scripts/admin/packageInsert.js
@@ -1,27 +1,29 @@
 Vue.component('packageInsert', {
     template: `
-        <div class="bg-white position-fixed p-4 window-insert shadow-lg rounded">
-            <div class="d-flex mb-3"><h5 class="ms-1 fw-bold flex-fill">Package</h5>
-                <button class="btn mt-0 pt-0 px-0" @click="close">
-                    <i class="bi bi-x text-danger"></i>
-                </button>
-            </div>
-            <div class="d-flex mb-3">
-                <div class="position-relative flex-fill me-2">
-                    <label class="ms-2 px-1 fw-light bg-white position-absolute input-label">Id</label>
-                    <input class="form-control" v-model="package.id" placeholder="Id">
+        <div class="position-fixed window-insert">
+            <div class="bg-white p-4 shadow-lg rounded">
+                <div class="d-flex mb-3"><h5 class="ms-1 fw-bold flex-fill">Package</h5>
+                    <button class="btn mt-0 pt-0 px-0" @click="close">
+                        <i class="bi bi-x text-danger"></i>
+                    </button>
                 </div>
-                <div class="position-relative flex-fill me-2">
-                    <label class="ms-2 px-1 fw-light bg-white position-absolute input-label">Label</label>
-                    <input class="form-control" v-model="package.label">
+                <div class="d-flex mb-3">
+                    <div class="position-relative flex-fill me-2">
+                        <label class="ms-2 px-1 fw-light bg-white position-absolute input-label">Id</label>
+                        <input class="form-control" v-model="package.id" placeholder="Id">
+                    </div>
+                    <div class="position-relative flex-fill me-2">
+                        <label class="ms-2 px-1 fw-light bg-white position-absolute input-label">Label</label>
+                        <input class="form-control" v-model="package.label">
+                    </div>
+                    <div class="position-relative flex-fill">
+                        <label class="ms-2 px-1 fw-light bg-white position-absolute input-label">Short</label>
+                        <input class="form-control" v-model="package.short">
+                    </div>
                 </div>
-                <div class="position-relative flex-fill">
-                    <label class="ms-2 px-1 fw-light bg-white position-absolute input-label">Short</label>
-                    <input class="form-control" v-model="package.short">
+                <div class="input-group">
+                    <button class="btn btn-primary form-control" v-on:click='insertOrUpdate'>Insert or Update</button>
                 </div>
-            </div>
-            <div class="input-group">
-                <button class="btn btn-primary form-control" v-on:click='insertOrUpdate'>Insert or Update</button>
             </div>
         </div>
     `,
