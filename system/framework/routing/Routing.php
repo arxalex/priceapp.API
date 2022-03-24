92ec5c76916bcba930a2ef4914d61d38d0300c56
Oleksandr Yushchenko <arxalex@arxalex.com>
2022-03-24 23:33:24 +0200

    add cookie to be

--- system/framework/routing/Routing.php
+++ system/framework/routing/Routing.php
@@ -72,7 +72,40 @@ class Routing
             } elseif ($requestType === "POST") {
                 $className = self::ENDPOINT_NAMESPACE . $class['className'];
                 $classObject = new $className();
-                $classObject->render(json_decode(file_get_contents("php://input"), true));
+
+                $params = json_decode(file_get_contents("php://input"), true);
+                $cookie = [];
+                if(isset($_COOKIE["userid"]) 
+                    && isset($_COOKIE["token"]) 
+                    && isset($_COOKIE["token_expires"])){
+                    $tokenService = new TokensService();
+                    $userid = $_COOKIE["userid"];
+                    $token = $_COOKIE["token"];
+                    $expires = $_COOKIE["token_expires"];
+
+                    $isValid = $tokenService->isTokenValid($userid, $token, $expires);
+
+                    if($isValid) {
+                        if($expires - 86400 < time()){
+                            $newToken = $tokenService->reValidToken($userid, $token, $expires);
+                            $token = $newToken->token;
+                            $expires = $newToken->expires;
+
+                            setcookie("userid", $userid, time() + 2592000, '/');
+                            setcookie("token", $token, $expires, '/');
+                            setcookie("token_expires", $expires, $expires, '/');
+                        }
+                        $cookie['userid'] = $userid;
+                        $cookie['token'] = $token;
+                        $cookie['token_expires'] = $expires;
+                    } else {
+                        http_response_code(403);
+                    }
+                }
+
+                $params['cookie'] = $cookie;
+
+                $classObject->render($params);
             } else {
                 http_response_code(404);
             }
