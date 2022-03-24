0426a9e0d01d6e596cd3a2e22ff4d2bfe7e49959
Oleksandr Yushchenko <arxalex@arxalex.com>
2022-03-24 22:33:29 +0200

    test

--- system/framework/entities/users/UsersService.php
+++ system/framework/entities/users/UsersService.php
@@ -93,7 +93,7 @@ class UsersService extends DefaultEntitiesService
             'username' => [ $username ],
             'password' => [ password_hash($password, PASSWORD_DEFAULT) ]
         ]);
-        echo password_hash($password, PASSWORD_DEFAULT);
+        error_log(password_hash($password, PASSWORD_DEFAULT));
         if(count($usersFromDB) != 1){
             header("Location: /login", true);
             die();
