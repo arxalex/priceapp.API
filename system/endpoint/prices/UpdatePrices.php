9d43a9b03f26071d19ded585f122565b711bd87e
Oleksandr Yushchenko <arxalex@arxalex.com>
2022-03-27 21:45:47 +0300

    test

--- system/endpoint/prices/UpdatePrices.php
+++ system/endpoint/prices/UpdatePrices.php
@@ -52,7 +52,7 @@ class UpdatePrices extends BaseEndpointBuilder
     {
         $from = $this->getParam('from');
         $to = $this->getParam('to');
-        set_time_limit(1200);
+        error_log(set_time_limit(1200));
         $result = new stdClass();
         $this->_usersService->unavaliableRequest($this->getParam('cookie'));
 
