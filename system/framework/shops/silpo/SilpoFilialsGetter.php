329a667806f3e0ed11349cd5d3445f88bb7b2cae
Oleksandr Yushchenko <arxalex@arxalex.com>
2022-03-27 18:40:37 +0300

    try curl

--- system/framework/shops/silpo/SilpoFilialsGetter.php
+++ system/framework/shops/silpo/SilpoFilialsGetter.php
@@ -14,10 +14,36 @@ class SilpoFilialsGetter
         $this->_fillialsService = new FilialsService();
     }
 
-    public function getFilials(string $token = 
-    "Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwNjJiM2Y1Ni0xZTJhLTQ4MzktYmVkYS04MTNmYzMwZmU1NmQiLCJ0eXAiOiJBY2Nlc3MiLCJzdWIiOiIwODQwY2NlOC03NWRjLTQyZjUtODUwNy00MjE2OGMyZjJhMjMiLCJmel9pdiI6Ilp3MmR3Y2RmVS9PbWJNeVJCblN2QlE9PSIsImZ6X3VzZXIiOiJBbFdlZkpjaWFBUkl5VStxamlZQ0lBPT0iLCJleHAiOjE2NDg0NjQyNDQsImlzcyI6ImZ6Z2xvYmFsL2FwaSJ9.O2cXrTv1xiuhwTOA3mVXJDyZ7Wzeq2plyW62OzRzarI"
-    ) : array
-    {
+    public function getFilials(
+        string $token =
+        "Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwNjJiM2Y1Ni0xZTJhLTQ4MzktYmVkYS04MTNmYzMwZmU1NmQiLCJ0eXAiOiJBY2Nlc3MiLCJzdWIiOiIwODQwY2NlOC03NWRjLTQyZjUtODUwNy00MjE2OGMyZjJhMjMiLCJmel9pdiI6Ilp3MmR3Y2RmVS9PbWJNeVJCblN2QlE9PSIsImZ6X3VzZXIiOiJBbFdlZkpjaWFBUkl5VStxamlZQ0lBPT0iLCJleHAiOjE2NDg0NjQyNDQsImlzcyI6ImZ6Z2xvYmFsL2FwaSJ9.O2cXrTv1xiuhwTOA3mVXJDyZ7Wzeq2plyW62OzRzarI"
+    ): array {
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.sm.silpo.ua/api/2.0/exec/FZGlobal/',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => '{"method": "GetFilials"}',
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwNjJiM2Y1Ni0xZTJhLTQ4MzktYmVkYS04MTNmYzMwZmU1NmQiLCJ0eXAiOiJBY2Nlc3MiLCJzdWIiOiIwODQwY2NlOC03NWRjLTQyZjUtODUwNy00MjE2OGMyZjJhMjMiLCJmel9pdiI6Ilp3MmR3Y2RmVS9PbWJNeVJCblN2QlE9PSIsImZ6X3VzZXIiOiJBbFdlZkpjaWFBUkl5VStxamlZQ0lBPT0iLCJleHAiOjE2NDg0NjQyNDQsImlzcyI6ImZ6Z2xvYmFsL2FwaSJ9.O2cXrTv1xiuhwTOA3mVXJDyZ7Wzeq2plyW62OzRzarI',
+                'Content-Type: application/json'
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        echo $response;
+
+        /*
+
         $url = 'https://api.sm.silpo.ua/api/2.0/exec/FZGlobal/';
         $data = json_encode([
             'method' => 'GetFilials'
@@ -25,18 +51,20 @@ class SilpoFilialsGetter
 
         $options = [
             'http' => [
-                'header' => ["authorization: $token\r\n", 
-                            "Content-Type: application/json;charset=UTF-8\r\n"],
+                'header' => "authorization: $token\r\n" .
+                    "Content-Type: application/json;charset=UTF-8\r\n",
                 'method'  => 'POST',
                 'content' => $data
             ]
         ];
         $context  = stream_context_create($options);
-        $result = json_decode(file_get_contents($url, false, $context));
+        $response = file_get_contents($url, false, $context)
+        */
+        $result = json_decode($response);
 
         return $result->filials;
     }
-    public function convertFilial(object $filialSilpo) : Filial
+    public function convertFilial(object $filialSilpo): Filial
     {
         return new Filial(
             null,
