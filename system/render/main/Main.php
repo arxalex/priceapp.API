a9a60c095a86eb419f5406572922ddf88a779a72
Oleksandr Yushchenko <arxalex@arxalex.com>
2022-05-02 23:40:12 +0300

    add home page

--- system/render/main/Main.php
+++ system/render/main/Main.php
@@ -15,10 +15,21 @@ class Main extends BaseRenderBuilder
     public function build()
     {
         return '
-        <div>
-        
+        <div class="row">
+            <div class="col-md-6 my-5 py-5">
+                <h1>Цей додаток зараз розробляється</h1>
+                <p>Слідкуйте за оновленнями</p>
+                <div class="mb-3">
+                    <a class="btn btn-success" href="http://eepurl.com/h04OAD">Підписатись на розсилку</a>
+                </div>
+                <div>
+                    <a href="https://t.me/price_app"><i class="bi bi-telegram"></i></a>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <img src="/public_resources/icons/priceapp_icon.svg">
+            </div>
         </div>
         ';
     }
 }
-?>
